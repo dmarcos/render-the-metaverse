d348cab9d97a35e8a565b81747bce36824de6390
Diego Marcos <diego.marcos@gmail.com>
2020-10-14 10:48:07 -0700

    Remove unnecessary logging

--- cubemap-switcher.js
+++ cubemap-switcher.js
@@ -213,5 +213,5 @@ function getQueryVariable (variable) {
       return decodeURIComponent(pair[1]);
     }
   }
-  console.log('Query variable %s not found', variable);
+  // console.log('Query variable %s not found', variable);
 }
